488d026a70ae92f1e137e91b24eee475b98b4e51
danielzehima <danielzehia@gail.com>
2025-10-29 15:51:10 +0000

    ajouter fichier leçon1_tkinter

---
+++ Leçon1-tkinter.py
@@ -0,0 +1,13 @@
+#importation de la biblithèque tkinter
+
+import tkinter as tk
+
+#création d'une fenêtre
+fenetre=tk.Tk()
+
+#Modification du titre de  la fenêtre
+fenetre.title("Première fenêtre")
+fenetre.geometry("400x300")# dimension largeurxhauteur
+
+#lancer la boucle fenetre obligatoire
+fenetre.mainloop()
